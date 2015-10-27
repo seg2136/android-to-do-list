2c75ea081e3d9249bdd1866d7baa9e93313902bb
Stephany Garcia <seg2136@gmail.com>
2015-10-27 11:48:52 -0700

    Task class import

---
+++ app/src/main/java/com/example/guest/todolist/models/task.java
@@ -0,0 +1,40 @@
+package com.example.guest.todolist.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+import java.util.List;
+
+/**
+ * Created by Guest on 10/27/15.
+ */
+
+@Table(name = "Tasks", id = "_id")
+public class Task extends Model {
+
+    @Column(name = "Description")
+    private String mDescription;
+
+    public Task() {
+        super();
+    }
+
+    public Task(String description) {
+        super();
+        mDescription = description;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public static List<Task> all() {
+        return new Select().from(Task.class).execute();
+    }
+
+}
